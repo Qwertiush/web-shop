0b527ae5fc28f9c0075f77501f52a196082cc54f
Qwertiush <pawel.rycerzk01@gmail.com>
2026-03-08 00:32:44 +0100

    updated config for cypres testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
 
   e2e: {
     baseUrl: "http://localhost:5173",
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
